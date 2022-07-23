a0b68c7b553b31ddb5d689f42f97e38e3b4142b9
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2022-07-23 17:15:02 +0530

    Sort

--- vega-lite-api/bar/src/viz.js
+++ vega-lite-api/bar/src/viz.js
@@ -3,7 +3,7 @@ import vl from "vega-lite-api";
 export const viz = vl
   .markBar()
   .encode(
-    vl.x().fieldN("country"),
+    vl.x().fieldN("country").sort("y"),
     vl.y().fieldQ("population").scale({ zero: false }),
     vl.color().fieldN("religion"),
     vl.tooltip().fieldN("population")
